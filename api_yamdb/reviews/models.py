6ab1ae2cbcacde5001fc4d02843351ea4c6fc720
Alexander Khasanov <AlexanderKhasanov1205@yandex.ru>
2023-07-04 01:55:28 +0300

    add models: Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -71,3 +71,85 @@ class Comment(models.Model):
 
     def __str__(self) -> str:
         return self.text[:30]
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название категории',
+        help_text='Укажите название категории (не более 256 символов)'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Уникальное имя категории',
+        help_text='Укажите уникальное имя категории (не более 50 символов)'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self) -> str:
+        return self.name[:30]
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название жанра',
+        help_text='Укажите название жанра (не более 256 символов)'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Уникальное имя жанра',
+        help_text='Укажите уникальное имя жанра (не более 50 символов)'
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self) -> str:
+        return self.name[:30]
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название произведения',
+        help_text='Введите название произведения (не более 256 символов)'
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        help_text='Укажите год выпуска произведения'
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание произведения',
+        help_text='Укажите описание произведения'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='category',
+        verbose_name='Категория произведения',
+        help_text='Укажите, к какой категории относится произведение'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='genre',
+        verbose_name='Жанр произведения',
+        help_text='Укажите, к какому жанру относится произведение'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self) -> str:
+        return self.name[:30]
